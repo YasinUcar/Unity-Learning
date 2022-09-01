711ca96dfe10ea062a488be89c31c8aa15b0d3c6
Yasin Uçar <yasinucar@protonmail.com>
2022-09-02 00:09:09 +0300

    FaceTarget

--- 03ZombieRunner/Assets/Scripts/EnemyAI.cs
+++ 03ZombieRunner/Assets/Scripts/EnemyAI.cs
@@ -7,9 +7,11 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5f;
+    [SerializeField] float turnSpeed = 5f;
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
     bool isProvoked = false;
+
     Animator anim;
     void Start()
     {
@@ -35,6 +37,7 @@ public class EnemyAI : MonoBehaviour
     }
     private void EngageTarget()
     {
+        FaceTarget();
         if (distanceToTarget >= navMeshAgent.stoppingDistance)
         {
             ChaseTarget();
@@ -58,7 +61,11 @@ public class EnemyAI : MonoBehaviour
     }
     private void FaceTarget()
     {
-
+        //normalized =  Vector3 değişkenini alıp, x,y,z bileşen değerlerini vektörün boyutu 1 birim olacak şekilde hesaplar. Vectorün sadece büyüklüğünü değiştirir, yönünde bir değişiklik olmaz. kısaca hızında bir değişiklik yapmaz
+        //normalized  = https://www.youtube.com/watch?v=NiKxi8kWqNk
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed); //Slerp daha yumuşak geçişler için kullanılır 
     }
     void OnDrawGizmosSelected()
     {

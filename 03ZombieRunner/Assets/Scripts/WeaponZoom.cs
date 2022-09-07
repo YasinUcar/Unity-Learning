44a0b8bb76fb39c6e7b1734a63d305275db0f17e
DESKTOP-A2FF8QR\Arda Atakan Gürses <yasinucar>
2022-09-07 12:42:07 +0300

    mouseSens

--- 03ZombieRunner/Assets/Scripts/WeaponZoom.cs
+++ 03ZombieRunner/Assets/Scripts/WeaponZoom.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     Camera cam;
     [SerializeField] float zoomOutFOV = 60f;
     [SerializeField] float zoomInFOV = 20f;
+    [SerializeField] float zoomOutSensivity = 2f;
+    [SerializeField] float zoomInSensivity = .5f;
+
     bool zoomInToogle = false;
+    RigidbodyFirstPersonController rigidbodyFirstPersonController;
+
     void Start()
     {
         cam = Camera.main;
+        rigidbodyFirstPersonController = GetComponent<RigidbodyFirstPersonController>();
     }
 
 
@@ -21,13 +28,19 @@ public class WeaponZoom : MonoBehaviour
         {
             if (!zoomInToogle)
             {
+                rigidbodyFirstPersonController.mouseLook.XSensitivity = zoomInSensivity;
+                rigidbodyFirstPersonController.mouseLook.YSensitivity = zoomInSensivity;
                 zoomInToogle = true;
                 WeaponZooming(zoomInFOV);
+
             }
             else
             {
+                rigidbodyFirstPersonController.mouseLook.XSensitivity = zoomOutSensivity;
+                rigidbodyFirstPersonController.mouseLook.YSensitivity = zoomOutSensivity;
                 zoomInToogle = false;
                 WeaponZooming(zoomOutFOV);
+
             }
         }
 

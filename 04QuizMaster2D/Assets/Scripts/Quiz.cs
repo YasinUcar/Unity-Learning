c1fd08e60282757bcc788fc163ce9a8342504873
DESKTOP-A2FF8QR\Arda Atakan Gürses <yasinucar>
2022-09-14 22:06:14 +0300

    Random Next Questin Timer

--- 04QuizMaster2D/Assets/Scripts/Quiz.cs
+++ 04QuizMaster2D/Assets/Scripts/Quiz.cs
@@ -6,41 +6,116 @@ using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI questionText;
 
-    [SerializeField] QuestionSO question;
+    [Header("Questions")]
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] List<QuestionSO> questions = new List<QuestionSO>();
+    [Header("Answers")]
     [SerializeField] GameObject[] answerButtons;
     int correctAnswerIndex;
+    bool hasAnsweredEarly;
+
+    [Header("Buttons Collor")]
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
 
+    [Header("Timer")]
+    [SerializeField] Image timerImage;
+    Timer timer;
+
+    QuestionSO currentQuestion;
     void Start()
     {
-        questionText.text = question.GetQuestion();
-        for (int i = 0; i < answerButtons.Length; i++)
-        {
-            TextMeshProUGUI buttonText2 = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText2.text = question.GetAnswer(i);
-        }
-
+        timer = GameObject.FindGameObjectWithTag("Timer").GetComponent<Timer>();
     }
-    public void OnAnswerSelected(int index)
+    void Update()
+    {
+        timerImage.fillAmount = timer.fillFraction;
+        if (timer.loadNextQuestion)
+        {
+            hasAnsweredEarly = false;
+            GetNextQuestion();
+            timer.loadNextQuestion = false;
+        }
+        else if (!hasAnsweredEarly && !timer.isAnsweringQuestion) //doğru cevabı gösterdiği durumda tıklamayı engelliyoruz
+        {
+            DisplayAnswer(-1);//else durumuna düşmesini sağlıyoruz
+            SetButtonState(false);
+        }
+    }
+    void DisplayAnswer(int index)
     {
         Image buttonImage;
-        if (index == question.GetCorrectAnswerIndex())
+        if (index == currentQuestion.GetCorrectAnswerIndex())
         {
             questionText.text = "Doğru!";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+
         }
         else
         {
-            correctAnswerIndex = question.GetCorrectAnswerIndex();
-            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            correctAnswerIndex = currentQuestion.GetCorrectAnswerIndex();
+            string correctAnswer = currentQuestion.GetAnswer(correctAnswerIndex);
             questionText.text = "Doğru cevap\n" + correctAnswer;
             buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
             answerButtons[index].GetComponent<Image>().color = Color.red;
+        }
+    }
+    public void OnAnswerSelected(int index)
+    {
+        hasAnsweredEarly = true;
+        DisplayAnswer(index);
+        SetButtonState(false);
+        timer.CancelTimer();
+    }
+    void GetNextQuestion()
+    {
+        if (questions.Count > 0)
+        {
+            SetButtonState(true);
+            SetDefaultButtonSprites();
+            GetRandomQuestion();
+            DisplayQuestion();
+        }
+
+    }
+    void GetRandomQuestion()
+    {
+        int randomIndex = Random.Range(0, questions.Count);
+        currentQuestion = questions[randomIndex];
+        //hata ayıklama 
+        if (questions.Contains(currentQuestion)) //listede mevcutsa çeşitli hataların önüne geçiyoruz
+        {
+            questions.Remove(currentQuestion); // old question remove
+        }
+    }
+    void DisplayQuestion()
+    {
+        questionText.text = currentQuestion.GetQuestion();
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText2 = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText2.text = currentQuestion.GetAnswer(i);
+        }
+
+    }
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+    void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
+            answerButtons[i].GetComponent<Image>().color = Color.white;
         }
     }
 }
---
+++ 04QuizMaster2D/Assets/Scripts/Timer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+    [SerializeField] GameObject Image;
+    public bool loadNextQuestion;
+    public float fillFraction;
+    public bool isAnsweringQuestion;
+
+    float timerValue;
+    void Update()
+    {
+        UpdateTimer();
+    }
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+        if (isAnsweringQuestion)
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
+            }
+        }
+    }
+}
